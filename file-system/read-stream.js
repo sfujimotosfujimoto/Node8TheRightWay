7d077c5b2cc44c458826551d868d48d96dfefc21
S Fujimoto <unicoshun@gmail.com>
2017-08-05 21:21:30 +0900

    first

---
+++ file-system/read-stream.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+require('fs').createReadStream(process.argv[2])
+  .on('data', chunk => process.stdout.write(`${chunk}\n`))
+  .on('error', err => process.stderr.write(`ERROR: ${err.message}\n`));
+
---
+++ file-system/read-sync.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const data = fs.readFileSync('target.txt');
+process.stdout.write(data.toString());
+
