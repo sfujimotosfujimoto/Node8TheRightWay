cdb2df7d3494d0e93f0e0e22761ab5c8c7f86f66
S Fujimoto <unicoshun@gmail.com>
2017-08-07 21:23:53 +0900

    ~p51

--- file-system/read-stream.js
+++ file-system/read-stream.js
@@ -1,6 +1,12 @@
-#!/usr/bin/env node
+
 'use strict';
 require('fs').createReadStream(process.argv[2])
-  .on('data', chunk => process.stdout.write(`${chunk}\n`))
+  .on('data', chunk => console.log(chunk.toString()))
   .on('error', err => process.stderr.write(`ERROR: ${err.message}\n`));
 
+//
+// 'use strict';
+// require('fs').createReadStream(process.argv[2])
+//   .on('data', chunk => process.stdout.write(`${chunk}\n`))
+//   .on('error', err => process.stderr.write(`ERROR: ${err.message}\n`));
+//
