d308b45ede5a3d5a04e3c400bed1be24e34ab732
S Fujimoto <unicoshun@gmail.com>
2017-08-07 19:58:18 +0900

    ~p45

---
+++ networking/test/ldj-client-test.js
@@ -0,0 +1,24 @@
+'use strict';
+const assert = require('assert');
+const EventEmitter = require('events').EventEmitter;
+const LDJClient = require('../lib/ldj-client');
+
+describe('LDJClient', () => {
+  let stream = null;
+  let client = null;
+
+  beforeEach(() => {
+    stream = new EventEmitter();
+    client = new LDJClient(stream);
+
+  });
+
+  it('should emit a message event from single data event', done => {
+    client.on('message', message => {
+      assert.deepEqual(message, {foo: 'bar'});
+      done();
+    });
+    stream.emit('data', '{"foo":');
+    process.nextTick(() => stream.emit('data', '"bar"}\n'));
+  });
+});
