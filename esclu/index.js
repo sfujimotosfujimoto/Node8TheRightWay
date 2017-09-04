0e49fd2535c2dc4c18ff961af66d6fe9672d0076
S Fujimoto <unicoshun@gmail.com>
2017-09-04 09:52:28 +0900

    databases elasticsearch

---
+++ esclu/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const program = require('commander');
+const pkg = require('./package.json');
+
+const fullUrl = (path = '') => {
+  let url = `http://${program.host}:${program.port}/`;
+  if (program.index) {
+    url += program.index + '/';
+    if (program.type) {
+      url += program.type + '/';
+    }
+  }
+  return url + path.replace(/^\/*/, '');
+};
+
+program
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage('[options] <command> [...]')
+  .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+  .option('-p, --port <number>', 'port number [9200]', '9200')
+  .option('-j, --json', 'format output as JSON')
+  .option('-i, --index <name>', 'which index to use')
+  .option('-t, --type <type>', 'default type for bulk operations');
+
+program
+  .command('url [path]')
+  .description('generate the URL for the options and path (default is /)')
+  .action((path = '/') => console.log(fullUrl(path)));
+
+program
+  .command('get [path]')
+  .description('perform an HTTP GET request for path (default is /)')
+  .action((path = '/') => {
+    const options = {
+      url: fullUrl(path),
+      json: program.json,
+    };
+    request(options, (err, res, body) => {
+      if (program.json) {
+        console.log(JSON.stringify(err || body));
+      } else {
+        if (err) throw err;
+        console.log(body);
+      }
+    });
+  });
+  
+
+program.parse(process.argv);
+
+if (!program.args.filter(arg => typeof arg === 'object').length) {
+  program.help();
+}
+
